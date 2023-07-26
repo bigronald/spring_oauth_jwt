ed521608ffa86146f28c3d4d56eda4b2244dc39a
Tinotenda Ronald Muchena <whyron32@gmail.com>
2023-07-26 10:00:42 +0200

    OAuth(Authorization, Resource and Client Server)

---
+++ Oauth2ResourceServer/src/main/java/springoauth/controller/HomeController.java
@@ -0,0 +1,17 @@
+package springoauth.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		LocalDateTime time = LocalDateTime.now();
+		return "Tokutendai nekukwanisa kusvika nenguva idzo! - " + time;
+	}
+
+}
